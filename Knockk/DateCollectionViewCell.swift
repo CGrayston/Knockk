09a4668b2c703c76e4903b4fef5cf248070d4b31
Chris Grayston <chris.grayston95@gmail.com>
2020-01-13 20:39:45 -0800

    Added custom working calandar

---
+++ Knockk/DateCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  DateCollectionViewCell.swift
+//  Knockk
+//
+//  Created by Chris Grayston on 1/10/20.
+//  Copyright © 2020 Chris Grayston. All rights reserved.
+//
+
+import UIKit
+
+class DateCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+}
